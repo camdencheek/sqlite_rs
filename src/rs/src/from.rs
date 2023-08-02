8ccdadc43c57960025d4a51b077ad1f211d9fb59
Camden Cheek <camden@ccheek.com>
2023-08-02 13:06:38 -0600

    clean up some comment styles

--- src/rs/src/from.rs
+++ src/rs/src/from.rs
@@ -9,47 +9,57 @@ use crate::select::Select;
 use crate::table::Table;
 use crate::util::bitmask::Bitmask;
 
-/*
-** The SrcItem object represents a single term in the FROM clause of a query.
-** The SrcList object is mostly an array of SrcItems.
-**
-** The jointype starts out showing the join type between the current table
-** and the next table on the list.  The parser builds the list this way.
-** But sqlite3SrcListShiftJoinType() later shifts the jointypes so that each
-** jointype expresses the join between the table and the previous table.
-**
-** In the colUsed field, the high-order bit (bit 63) is set if the table
-** contains more than 63 columns and the 64-th or later column is used.
-**
-** Union member validity:
-**
-**    u1.zIndexedBy          fg.isIndexedBy && !fg.isTabFunc
-**    u1.pFuncArg            fg.isTabFunc   && !fg.isIndexedBy
-**    u2.pIBIndex            fg.isIndexedBy && !fg.isCte
-**    u2.pCteUse             fg.isCte       && !fg.isIndexedBy
-*/
+/// The SrcItem object represents a single term in the FROM clause of a query.
+/// The SrcList object is mostly an array of SrcItems.
+///
+/// The jointype starts out showing the join type between the current table
+/// and the next table on the list.  The parser builds the list this way.
+/// But sqlite3SrcListShiftJoinType() later shifts the jointypes so that each
+/// jointype expresses the join between the table and the previous table.
+///
+/// In the colUsed field, the high-order bit (bit 63) is set if the table
+/// contains more than 63 columns and the 64-th or later column is used.
+///
+/// Union member validity:
+///
+///    u1.zIndexedBy          fg.isIndexedBy && !fg.isTabFunc
+///    u1.pFuncArg            fg.isTabFunc   && !fg.isIndexedBy
+///    u2.pIBIndex            fg.isIndexedBy && !fg.isCte
+///    u2.pCteUse             fg.isCte       && !fg.isIndexedBy
 #[repr(C)]
 pub struct SrcItem {
-    pSchema: *mut Schema,   /* Schema to which this item is fixed */
-    zDatabase: *mut c_char, /* Name of database holding this table */
-    zName: *mut c_char,     /* Name of the table */
-    zAlias: *mut c_char,    /* The "B" part of a "A AS B" phrase.  zName is the "A" */
-    pTab: *mut Table,       /* An SQL table corresponding to zName */
-    pSelect: *mut Select,   /* A SELECT statement used in place of a table name */
-    addrFillSub: c_int,     /* Address of subroutine to manifest a subquery */
-    regReturn: c_int,       /* Register holding return address of addrFillSub */
-    regResult: c_int,       /* Registers holding results of a co-routine */
+    /// Schema to which this item is fixed */
+    pSchema: *mut Schema,
+    /// Name of database holding this table */
+    zDatabase: *mut c_char,
+    /// Name of the table
+    zName: *mut c_char,
+    /// The "B" part of a "A AS B" phrase.  zName is the "A"
+    zAlias: *mut c_char,
+    /// An SQL table corresponding to zName
+    pTab: *mut Table,
+    /// A SELECT statement used in place of a table name
+    pSelect: *mut Select,
+    /// Address of subroutine to manifest a subquery
+    addrFillSub: c_int,
+    /// Register holding return address of addrFillSub
+    regReturn: c_int,
+    /// Registers holding results of a co-routine
+    regResult: c_int,
     fg: SrcItem_fg,
-    iCursor: c_int, /* The VDBE cursor number used to access this table */
+    /// The VDBE cursor number used to access this table
+    iCursor: c_int,
     u3: SrcItem_u3,
-    colUsed: Bitmask, /* Bit N set if column N used. Details above for N>62 */
+    /// Bit N set if column N used. Details above for N>62
+    colUsed: Bitmask,
     u1: SrcItem_u1,
     u2: SrcItem_u2,
 }
 
 #[repr(C)]
 pub struct SrcItem_fg {
-    jointype: u8, /* Type of join between this table and the previous */
+    /// Type of join between this table and the previous
+    jointype: u8,
     // TODO: pack all these fields
     // unsigned notIndexed :1;
     // unsigned isIndexedBy :1;
@@ -65,38 +75,58 @@ pub struct SrcItem_fg {
     // unsigned isOn :1;
     // unsigned isSynthUsing :1;
     // unsigned isNestedFrom :1;
-    notIndexed: u8,     /* True if there is a NOT INDEXED clause */
-    isIndexedBy: u8,    /* True if there is an INDEXED BY clause */
-    isTabFunc: u8,      /* True if table-valued-function syntax */
-    isCorrelated: u8,   /* True if sub-query is correlated */
-    isMaterialized: u8, /* This is a materialized view */
-    viaCoroutine: u8,   /* Implemented as a co-routine */
-    isRecursive: u8,    /* True for recursive reference in WITH */
-    fromDDL: u8,        /* Comes from sqlite_schema */
-    isCte: u8,          /* This is a CTE */
-    notCte: u8,         /* This item may not match a CTE */
-    isUsing: u8,        /* u3.pUsing is valid */
-    isOn: u8,           /* u3.pOn was once valid and non-NULL */
-    isSynthUsing: u8,   /* u3.pUsing is synthensized from NATURAL */
-    isNestedFrom: u8,   /* pSelect is a SF_NestedFrom subquery */
+    /// True if there is a NOT INDEXED clause
+    notIndexed: u8,
+    /// True if there is an INDEXED BY clause
+    isIndexedBy: u8,
+    /// True if table-valued-function syntax
+    isTabFunc: u8,
+    /// True if sub-query is correlated
+    isCorrelated: u8,
+    /// This is a materialized view
+    isMaterialized: u8,
+    /// Implemented as a co-routine
+    viaCoroutine: u8,
+    /// True for recursive reference in WITH
+    isRecursive: u8,
+    /// Comes from sqlite_schema
+    fromDDL: u8,
+    /// This is a CTE
+    isCte: u8,
+    /// This item may not match a CTE
+    notCte: u8,
+    /// u3.pUsing is valid
+    isUsing: u8,
+    /// u3.pOn was once valid and non-NULL
+    isOn: u8,
+    /// u3.pUsing is synthensized from NATURAL
+    isSynthUsing: u8,
+    /// pSelect is a SF_NestedFrom subquery
+    isNestedFrom: u8,
 }
 
 #[repr(C)]
 pub union SrcItem_u3 {
-    pOn: *mut Expr,      /* fg.isUsing==0 =>  The ON clause of a join */
-    pUsing: *mut IdList, /* fg.isUsing==1 =>  The USING clause of a join */
+    /// fg.isUsing==0 =>  The ON clause of a join
+    pOn: *mut Expr,
+    /// fg.isUsing==1 =>  The USING clause of a join
+    pUsing: *mut IdList,
 }
 
 #[repr(C)]
 pub union SrcItem_u1 {
-    zIndexedBy: *mut c_char, /* Identifier from "INDEXED BY <zIndex>" clause */
-    pFuncArg: *mut ExprList, /* Arguments to table-valued-function */
+    /// Identifier from "INDEXED BY <zIndex>" clause
+    zIndexedBy: *mut c_char,
+    /// Arguments to table-valued-function
+    pFuncArg: *mut ExprList,
 }
 
 #[repr(C)]
 pub union SrcItem_u2 {
-    pIBIndex: *mut Index, /* Index structure corresponding to u1.zIndexedBy */
-    pCteUse: *mut CteUse, /* CTE Usage info when fg.isCte is true */
+    /// Index structure corresponding to u1.zIndexedBy
+    pIBIndex: *mut Index,
+    /// CTE Usage info when fg.isCte is true
+    pCteUse: *mut CteUse,
 }
 
 /// This object represents one or more tables that are the source of
--- src/rs/src/upsert.rs
+++ src/rs/src/upsert.rs
@@ -4,39 +4,51 @@ use crate::expr::{Expr, ExprList};
 use crate::from::SrcList;
 use crate::index::Index;
 
-/*
-** An instance of the following object describes a single ON CONFLICT
-** clause in an upsert.
-**
-** The pUpsertTarget field is only set if the ON CONFLICT clause includes
-** conflict-target clause.  (In "ON CONFLICT(a,b)" the "(a,b)" is the
-** conflict-target clause.)  The pUpsertTargetWhere is the optional
-** WHERE clause used to identify partial unique indexes.
-**
-** pUpsertSet is the list of column=expr terms of the UPDATE statement.
-** The pUpsertSet field is NULL for a ON CONFLICT DO NOTHING.  The
-** pUpsertWhere is the WHERE clause for the UPDATE and is NULL if the
-** WHERE clause is omitted.
-*/
+/// An instance of the following object describes a single ON CONFLICT
+/// clause in an upsert.
+///
+/// The pUpsertTarget field is only set if the ON CONFLICT clause includes
+/// conflict-target clause.  (In "ON CONFLICT(a,b)" the "(a,b)" is the
+/// conflict-target clause.)  The pUpsertTargetWhere is the optional
+/// WHERE clause used to identify partial unique indexes.
+///
+/// pUpsertSet is the list of column=expr terms of the UPDATE statement.
+/// The pUpsertSet field is NULL for a ON CONFLICT DO NOTHING.  The
+/// pUpsertWhere is the WHERE clause for the UPDATE and is NULL if the
+/// WHERE clause is omitted.
 #[repr(C)]
 pub struct Upsert {
-    pUpsertTarget: *mut ExprList, /* Optional description of conflict target */
-    pUpsertTargetWhere: *mut Expr, /* WHERE clause for partial index targets */
-    pUpsertSet: *mut ExprList,    /* The SET clause from an ON CONFLICT UPDATE */
-    pUpsertWhere: *mut Expr,      /* WHERE clause for the ON CONFLICT UPDATE */
-    pNextUpsert: *mut Upsert,     /* Next ON CONFLICT clause in the list */
-    isDoUpdate: u8,               /* True for DO UPDATE.  False for DO NOTHING */
-    /* Above this point is the parse tree for the ON CONFLICT clauses.
-     ** The next group of fields stores intermediate data. */
-    pToFree: *mut c_void, /* Free memory when deleting the Upsert object */
-    /* All fields above are owned by the Upsert object and must be freed
-     ** when the Upsert is destroyed.  The fields below are used to transfer
-     ** information from the INSERT processing down into the UPDATE processing
-     ** while generating code.  The fields below are owned by the INSERT
-     ** statement and will be freed by INSERT processing. */
-    pUpsertIdx: *mut Index, /* UNIQUE constraint specified by pUpsertTarget */
-    pUpsertSrc: *mut SrcList, /* Table to be updated */
-    regData: c_int,         /* First register holding array of VALUES */
-    iDataCur: c_int,        /* Index of the data cursor */
-    iIdxCur: c_int,         /* Index of the first index cursor */
+    /// Optional description of conflict target
+    pUpsertTarget: *mut ExprList,
+    /// WHERE clause for partial index targets
+    pUpsertTargetWhere: *mut Expr,
+    /// The SET clause from an ON CONFLICT UPDATE
+    pUpsertSet: *mut ExprList,
+    /// WHERE clause for the ON CONFLICT UPDATE
+    pUpsertWhere: *mut Expr,
+    /// Next ON CONFLICT clause in the list
+    pNextUpsert: *mut Upsert,
+    /// True for DO UPDATE.  False for DO NOTHING
+    isDoUpdate: u8,
+
+    // Above this point is the parse tree for the ON CONFLICT clauses.
+    // The next group of fields stores intermediate data.
+    /// Free memory when deleting the Upsert object
+    pToFree: *mut c_void,
+
+    // All fields above are owned by the Upsert object and must be freed
+    // when the Upsert is destroyed.  The fields below are used to transfer
+    // information from the INSERT processing down into the UPDATE processing
+    // while generating code.  The fields below are owned by the INSERT
+    // statement and will be freed by INSERT processing.
+    /// UNIQUE constraint specified by pUpsertTarget
+    pUpsertIdx: *mut Index,
+    /// Table to be updated
+    pUpsertSrc: *mut SrcList,
+    /// First register holding array of VALUES
+    regData: c_int,
+    /// Index of the data cursor
+    iDataCur: c_int,
+    /// Index of the first index cursor
+    iIdxCur: c_int,
 }
