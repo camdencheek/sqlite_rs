4dff55417ae597054e04f32f449ee05d353f717a
Camden Cheek <camden@ccheek.com>
2023-08-02 22:54:27 -0600

    move BtShared to rust

--- src/rs/src/btree/mod.rs
+++ src/rs/src/btree/mod.rs
@@ -1,8 +1,13 @@
 use libc::{c_int, c_void};
 
-use crate::{db::sqlite3, sqlite3_value};
+use crate::{
+    db::{sqlite3, sqlite3_mutex},
+    pager::Pager,
+    sqlite3_value,
+    util::bitvec::Bitvec,
+};
 
-use self::internal::BtLock;
+use self::internal::{BtLock, MemPage};
 
 pub mod internal;
 
@@ -66,12 +71,108 @@ pub struct BtCursor {
     _marker: core::marker::PhantomData<(*mut u8, core::marker::PhantomPinned)>,
 }
 
-/// Temporary opaque struct
-/// Using tricks from here: https://doc.rust-lang.org/nomicon/ffi.html#representing-opaque-structs
-// cbindgen:ignore
+/// An instance of this object represents a single database file.
+///
+/// A single database file can be in use at the same time by two
+/// or more database connections.  When two or more connections are
+/// sharing the same database file, each connection has it own
+/// private Btree object for the file and each of those Btrees points
+/// to this one BtShared object.  BtShared.nRef is the number of
+/// connections currently sharing this database file.
+///
+/// Fields in this structure are accessed under the BtShared.mutex
+/// mutex, except for nRef and pNext which are accessed under the
+/// global SQLITE_MUTEX_STATIC_MAIN mutex.  The pPager field
+/// may not be modified once it is initially set as long as nRef>0.
+/// The pSchema field may be set once under BtShared.mutex and
+/// thereafter is unchanged as long as nRef>0.
+///
+/// isPending:
+///
+///   If a BtShared client fails to obtain a write-lock on a database
+///   table (because there exists one or more read-locks on the table),
+///   the shared-cache enters 'pending-lock' state and isPending is
+///   set to true.
+///
+///   The shared-cache leaves the 'pending lock' state when either of
+///   the following occur:
+///
+///     1) The current writer (BtShared.pWriter) concludes its transaction, OR
+///     2) The number of locks held by other connections drops to zero.
+///
+///   while in the 'pending-lock' state, no connection may start a new
+///   transaction.
+///
+///   This feature is included to help prevent writer-starvation.
+#[repr(C)]
 pub struct BtShared {
-    _data: [u8; 0],
-    _marker: core::marker::PhantomData<(*mut u8, core::marker::PhantomPinned)>,
+    /// The page cache
+    pPager: *mut Pager,
+    /// Database connection currently using this Btree
+    db: *mut sqlite3,
+    /// A list of all open cursors
+    pCursor: *mut BtCursor,
+    /// First page of the database
+    pPage1: *mut MemPage,
+    /// Flags to sqlite3BtreeOpen()
+    openFlags: u8,
+    /// True if auto-vacuum is enabled
+    #[cfg(not(omit_autovacuum))]
+    autoVacuum: u8,
+    /// True if incr-vacuum is enabled
+    #[cfg(not(omit_autovacuum))]
+    incrVacuum: u8,
+    /// True to truncate db on commit
+    #[cfg(not(omit_autovacuum))]
+    bDoTruncate: u8,
+    /// Transaction state
+    inTransaction: u8,
+    /// Maximum first byte of cell for a 1-byte payload
+    max1bytePayload: u8,
+    /// Desired number of extra bytes per page
+    nReserveWanted: u8,
+    /// Boolean parameters.  See BTS_* macros below
+    btsFlags: u16,
+    /// Maximum local payload in non-LEAFDATA tables
+    maxLocal: u16,
+    /// Minimum local payload in non-LEAFDATA tables
+    minLocal: u16,
+    /// Maximum local payload in a LEAFDATA table
+    maxLeaf: u16,
+    /// Minimum local payload in a LEAFDATA table
+    minLeaf: u16,
+    /// Total number of bytes on a page
+    pageSize: u32,
+    /// Number of usable bytes on each page
+    usableSize: u32,
+    /// Number of open transactions (read + write)
+    nTransaction: c_int,
+    /// Number of pages in the database
+    nPage: u32,
+    /// Pointer to space allocated by sqlite3BtreeSchema()
+    pSchema: *mut c_void,
+    /// Destructor for BtShared.pSchema
+    xFreeSchema: unsafe extern "C" fn(*mut c_void),
+    /// Non-recursive mutex required to access this object
+    mutex: *mut sqlite3_mutex,
+    /// Set of pages moved to free-list this transaction
+    pHasContent: *mut Bitvec,
+    /// Number of references to this structure
+    #[cfg(not(omit_shared_cache))]
+    nRef: c_int,
+    /// Next on a list of sharable BtShared structs
+    #[cfg(not(omit_shared_cache))]
+    pNext: *mut BtShared,
+    /// List of locks held on this shared-btree struct
+    #[cfg(not(omit_shared_cache))]
+    pLock: *mut BtLock,
+    /// Btree with currently open write transaction
+    #[cfg(not(omit_shared_cache))]
+    pWriter: *mut Btree,
+    /// Temp space sufficient to hold a single cell
+    pTmpSpace: *mut u8,
+    /// Size of last cell written by TransferRow()
+    nPreformatSize: c_int,
 }
 
 /// An instance of the BtreePayload object describes the content of a single
